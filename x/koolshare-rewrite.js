dd8589214e6f7cc13d3e1a576e126ce971c0e097
Kangyi Cui <cuikangyi@gmail.com>
2021-04-08 08:28:09 +0800

    update

--- x/koolshare-rewrite.js
+++ x/koolshare-rewrite.js
@@ -1,6 +1,11 @@
 
+var url = $request.url;
 var body = $response.body;
 
-body = body.replace(/forum.php\?mod=forumdisplay&(amp;)?fid=(\d+)&(amp;)?mobile=1/g, 'forum.php?mod=forumdisplay&fid=$2&filter=author&orderby=dateline&mobile=1')
+if (url.indexOf('mod') < 0) {
+	body = body.replace(/forum.php\?mod=forumdisplay&(amp;)?fid=(\d+)&(amp;)?mobile=1/g, 'forum.php?mod=forumdisplay&fid=$2&filter=author&orderby=dateline&mobile=1')
+}
+
+body = body.replace('fm.src', 'fm.src="";//');
 
 $done(body);
