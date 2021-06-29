c4f4d4a67f9c2fb9685825a6497cfa0b0038e700
Kangyi Cui <cuikangyi@outlook.com>
2021-06-29 22:21:50 +0800

    Update koolshare-rewrite.js

--- x/koolshare-rewrite.js
+++ x/koolshare-rewrite.js
@@ -6,6 +6,6 @@ if (url.indexOf('mod') < 0) {
 	body = body.replace(/forum.php\?mod=forumdisplay&(amp;)?fid=(\d+)&(amp;)?mobile=1/g, 'forum.php?mod=forumdisplay&fid=$2&filter=author&orderby=dateline&mobile=1')
 }
 
-body = body.replace('fm.src', 'fm.src="";//');
+//body = body.replace('fm.src', 'fm.src="";//');
 
 $done(body);
